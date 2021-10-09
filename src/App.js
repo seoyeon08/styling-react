38ca78a93ef8f6f1f759ac2e161519210e429790
seoyeon <silversun0808@gmail.com>
2021-10-10 01:19:35 +0900

    Add: CSS Module 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import SassComponent from './SassComponent';
+import React, { Componenet } from 'react';
+import CSSModule from './CSSModule';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <SassComponent />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<CSSModule />
+			</div>
+		);
+	}
 }
-
 export default App;
